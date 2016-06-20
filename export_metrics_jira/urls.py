4ca792781b275bd3e9b0cc9033aa7ca22b7c6f92
brunofr <brunofr@ciandt.com>
2016-06-20 09:23:55 -0300

    include gadget

--- export_metrics_jira/urls.py
+++ export_metrics_jira/urls.py
@@ -3,14 +3,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import root.views
 
 # Examples:
 # url(r'^$', 'export_metrics_jira.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', root.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
